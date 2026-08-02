f5768a977039ddc81d87daad59e3c68239e8db32
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-08-02 13:14:00 +0530

    max element

---
+++ src/test/java/ArrayPractice/maximumElement.java
@@ -0,0 +1,19 @@
+package ArrayPractice;
+
+public class maximumElement {
+	
+	public static void main(String[] args) {
+		int[] data = {23,3,4,54,2,56};
+		int maxElement = 0;
+		for(int i=0;i<data.length;i++) {
+			for(int j=0;j<data.length;j++) {
+				if(maxElement>=data[j]) {		
+				}else {
+					maxElement = data[j];
+				}
+			}
+		}
+		System.out.println(maxElement);
+	}
+
+}
