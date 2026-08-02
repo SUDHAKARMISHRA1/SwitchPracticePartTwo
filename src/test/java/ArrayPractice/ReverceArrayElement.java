0389afb57e5cb1952275a3541f3ecd386c8ac229
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-08-02 13:38:40 +0530

    Rev arry

---
+++ src/test/java/ArrayPractice/ReverceArrayElement.java
@@ -0,0 +1,19 @@
+package ArrayPractice;
+
+import java.lang.reflect.Array;
+
+public class ReverceArrayElement {
+
+	public static void main(String[] args) {
+		int[] data = {2,4,6,8,0,1,1};
+		int j=0;
+		int[] datarev= new int[data.length];
+		for(int i=data.length-1;i>=0;i--) {
+		datarev[j]= data[i];
+		j++;
+		}
+		for(int i=0;i<datarev.length;i++) {
+			System.out.println(datarev[i]);
+		}
+	}
+}
