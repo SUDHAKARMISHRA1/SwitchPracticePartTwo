7d5bf50fdd2ed9465c7089d0ddd1bd0dfd76ba96
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-08-08 11:29:55 +0530

    ArrayList general

---
+++ src/test/java/ArrayListPractice/GeneralProblem.java
@@ -0,0 +1,22 @@
+package ArrayListPractice;
+
+import java.util.ArrayList;
+
+public class GeneralProblem {
+	
+	public static void main(String[] args) {
+		ArrayList<Integer> data = new ArrayList<>();
+	
+		data.add(2);
+		data.add(1,2);
+		data.add(1,4);
+		data.add(1,3);
+		
+		data.remove(2);
+		
+		System.out.println(data.size());
+		
+		System.out.println(data);
+	}
+
+}
