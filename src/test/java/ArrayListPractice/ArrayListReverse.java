4a2b60e870f2bd1ed93981bb7b0b21a03cea2590
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-08-08 13:13:20 +0530

    Arraylist Reverse

---
+++ src/test/java/ArrayListPractice/ArrayListReverse.java
@@ -0,0 +1,29 @@
+package ArrayListPractice;
+
+import java.util.ArrayList;
+
+public class ArrayListReverse {
+	
+	public static void main(String[] args) {
+		ArrayList<Integer> data = new ArrayList<>();
+		ArrayList<Integer> revData = new ArrayList<>(data.size());
+		data.add(2);
+		data.add(1,2);
+		data.add(1,4);
+		data.add(1,3);
+		data.add(9);
+		
+		System.out.println(data);
+		//int index=0;
+		for (int i = 0; i < data.size() / 2; i++) {
+
+		    int temp = data.get(i);
+
+		    data.set(i, data.get(data.size() - 1 - i));
+
+		    data.set(data.size() - 1 - i, temp);
+		}
+		System.out.println(data);
+	}
+
+}
