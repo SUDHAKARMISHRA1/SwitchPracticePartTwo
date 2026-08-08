105e94bc46121c0ff12c9a28ae3999990bc2ea78
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-08-08 21:24:24 +0530

    More practice

--- src/test/java/ArrayListPractice/GeneralProblem.java
+++ src/test/java/ArrayListPractice/GeneralProblem.java
@@ -11,6 +11,10 @@ public class GeneralProblem {
 		data.add(1,2);
 		data.add(1,4);
 		data.add(1,3);
+		data.add(1,4);
+		data.add(1,3);
+		data.add(1,4);
+		data.add(1,3);
 		
 		data.remove(2);
 		
@@ -20,6 +24,20 @@ public class GeneralProblem {
 		
 		System.out.println(data.contains(3));
 		System.out.println(data.contains(9));
+		
+		ArrayList<Integer> data2 = new ArrayList<>();
+		System.out.println(data2.isEmpty());
+		System.out.println(data);
+		for(int c: data) {
+			System.out.println(c);
+		}
+		
+		data.remove(1);
+		data.add(2,4);
+		System.out.println(data);
+		
+		data2.addAll(data);
+		System.out.println(data2);
 	}
 
 }
