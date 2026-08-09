4f9cee6d25ef0d359258d1001e04a2917053e67e
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-08-09 17:55:54 +0530

    more

--- src/test/java/HashMapPractice/GeneralPractice.java
+++ src/test/java/HashMapPractice/GeneralPractice.java
@@ -9,12 +9,18 @@ public class GeneralPractice {
 		HashMap<Integer, String> empDetail= new HashMap<>();
 		empDetail.put(1, "Rohan");
 		empDetail.put(2, "King");
+		empDetail.put(3, "Kiing");
 		
 		System.out.println(empDetail);
 		
 		System.out.println(empDetail.get(2));
 		System.out.println(empDetail.containsKey(1));
 		System.out.println(empDetail.containsKey(3));
+		System.out.println(empDetail.containsValue("Rohan"));
+		System.out.println(empDetail.containsValue("king"));
+		empDetail.remove(3);
+		System.out.println(empDetail);
+		
 		
 		
 		
