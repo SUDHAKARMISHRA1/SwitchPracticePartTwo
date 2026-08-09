85850490b47dbc1bd126613456ef3bb6b6eaf483
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-08-09 17:51:22 +0530

    HashMap general

---
+++ src/test/java/HashMapPractice/GeneralPractice.java
@@ -0,0 +1,18 @@
+package HashMapPractice;
+
+import java.util.HashMap;
+
+public class GeneralPractice {
+	
+	public static void main(String[] args) {
+		
+		HashMap<Integer, String> empDetail= new HashMap<>();
+		empDetail.put(1, "Rohan");
+		empDetail.put(2, "King");
+		
+		System.out.println(empDetail);
+		
+		
+	}
+
+}
