28317560ad7043560a8eb452c0e51491d15c7ed1
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-08-09 18:15:18 +0530

    more

--- src/test/java/HashMapPractice/GeneralPractice.java
+++ src/test/java/HashMapPractice/GeneralPractice.java
@@ -1,6 +1,7 @@
 package HashMapPractice;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class GeneralPractice {
@@ -29,6 +30,30 @@ public class GeneralPractice {
 			System.out.println(c.getKey()+" : "+c.getValue());
 		}
 		
+		Iterator<Integer> it = empDetail.keySet().iterator();
+		while(it.hasNext()) {
+			int key= it.next();
+			String value = empDetail.get(key);
+			System.out.println("Key is: " +key + " and Value is: "+value);
+		}
+		
+		empDetail.clear();
+		System.out.println("Data is:" +empDetail);
+		System.out.println(empDetail.isEmpty());
+		
+		
+		HashMap<Integer, String> empDetail1= new HashMap<>();
+		empDetail1.put(1, "Rohan");
+		empDetail1.put(2, "King");
+		empDetail1.put(3, "Kiing");
+		
+		HashMap<Integer, String> empDetail2= new HashMap<>();
+		empDetail2.put(4, "Rohan2");
+		empDetail2.put(5, "King2");
+		empDetail2.put(3, "Kiing2");
+		
+		empDetail1.putAll(empDetail2);
+		System.out.println(empDetail1);
 		
 		
 		
