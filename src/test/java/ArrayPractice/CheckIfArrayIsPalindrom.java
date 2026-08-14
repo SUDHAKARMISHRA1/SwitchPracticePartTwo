8e1eb2640e3d733794b4b149799b267af81213d0
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-08-14 20:23:41 +0530

    palindrome

---
+++ src/test/java/ArrayPractice/CheckIfArrayIsPalindrom.java
@@ -0,0 +1,22 @@
+package ArrayPractice;
+
+public class CheckIfArrayIsPalindrom {
+	
+	public static void main(String[] args) {
+		int[] arr = {1,2,1,1};
+		 boolean isPalindrome = true;
+		for (int i = 0; i < arr.length / 2; i++) {
+
+            if (arr[i] != arr[arr.length - 1 - i]) {
+                isPalindrome = false;
+                break;
+            }
+        }
+		 if (isPalindrome) {
+	            System.out.println("Array is Palindrome");
+	        } else {
+	            System.out.println("Array is not Palindrome");
+	        }
+	}
+
+}
