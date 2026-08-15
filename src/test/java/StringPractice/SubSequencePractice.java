b675385541fc610d205daa202a94c11204d57053
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-08-15 22:09:59 +0530

    all Subsequence

---
+++ src/test/java/StringPractice/SubSequencePractice.java
@@ -0,0 +1,33 @@
+package StringPractice;
+
+import java.util.ArrayList;
+
+public class SubSequencePractice {
+
+	 static void printSubsequence(String data, int index, String result) {
+
+	        if (index == data.length()) {
+	            System.out.println(result);
+	            return;
+	        }
+
+	        // Don't take current character
+	        printSubsequence(data, index + 1, result);
+
+	        // Take current character
+	        printSubsequence(
+	            data,
+	            index + 1,
+	            result + data.charAt(index)
+	        );
+	    }
+
+	    public static void main(String[] args) {
+
+	        String data = "ABCD";
+
+	        printSubsequence(data, 0, "");
+	    
+}
+	
+}
