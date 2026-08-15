b7923df206de819e41c7c6c031a0f01bddd4943a
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-08-15 21:27:18 +0530

    substring pactice

---
+++ src/test/java/StringPractice/LongestSubstringWithoutRepeatingChar.java
@@ -0,0 +1,9 @@
+package StringPractice;
+
+public class LongestSubstringWithoutRepeatingChar {
+
+	public static void main(String[] args) {
+		String data = "abcgcdef";
+	
+	}
+}
--- src/test/java/StringPractice/SubStringPractice.java
+++ src/test/java/StringPractice/SubStringPractice.java
@@ -5,11 +5,15 @@ public class SubStringPractice {
 	public static void main(String[] args) {
 		String data = "abcd";
 		//all substring
+		int subStringCount = 0;
 		for(int i=0;i<data.length();i++) {
 			for(int j=i+1;j<=data.length();j++) {
+				subStringCount++;
 				System.out.println(data.substring(i,j));
 			}			
 		}
+		//SubString count
+		System.out.println("Total Substring count are: " +subStringCount);
 		
 		
 		
