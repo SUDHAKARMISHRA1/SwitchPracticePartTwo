1553552c0be2b05470556d6698fdfa38fb9ce313
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-08-15 21:17:39 +0530

    SubString practice

---
+++ src/test/java/StringPractice/SubStringPractice.java
@@ -0,0 +1,18 @@
+package StringPractice;
+
+public class SubStringPractice {
+	
+	public static void main(String[] args) {
+		String data = "abcd";
+		//all substring
+		for(int i=0;i<data.length();i++) {
+			for(int j=i+1;j<=data.length();j++) {
+				System.out.println(data.substring(i,j));
+			}			
+		}
+		
+		
+		
+	}
+
+}
