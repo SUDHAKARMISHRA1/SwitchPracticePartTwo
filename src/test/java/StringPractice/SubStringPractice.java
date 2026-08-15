93ea36d6044f2b36a40fb4f5c2ebdce2add494ce
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-08-15 21:31:20 +0530

    More practice

--- src/test/java/StringPractice/SubStringPractice.java
+++ src/test/java/StringPractice/SubStringPractice.java
@@ -4,17 +4,26 @@ public class SubStringPractice {
 	
 	public static void main(String[] args) {
 		String data = "abcd";
+		String inputSubstring = "cd";
 		//all substring
 		int subStringCount = 0;
 		for(int i=0;i<data.length();i++) {
 			for(int j=i+1;j<=data.length();j++) {
 				subStringCount++;
-				System.out.println(data.substring(i,j));
+			//	System.out.println(data.substring(i,j));
 			}			
 		}
 		//SubString count
 		System.out.println("Total Substring count are: " +subStringCount);
 		
+		//string is substring of other
+		for(int i=0;i<data.length();i++) {
+			for(int j=i+1;j<=data.length();j++) {
+				if(data.substring(i,j).equals(inputSubstring)) {
+					System.out.println("Given string is substring or other");
+				}
+			}			
+		}
 		
 		
 	}
