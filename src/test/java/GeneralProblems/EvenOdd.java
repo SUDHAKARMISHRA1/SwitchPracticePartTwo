c23b28f5c026ab5e81d3e338b02f7e7e4b7bf9e0
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-08-17 22:13:07 +0530

    Even odd

---
+++ src/test/java/GeneralProblems/EvenOdd.java
@@ -0,0 +1,20 @@
+package GeneralProblems;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int val =sc.nextInt();
+		
+		if(val%2==0) {
+			System.out.println("Number is Even");
+		}else {
+			System.out.println("Number is odd");
+		}
+	}
+
+}
