25be42c659d82b5868acdbe4ff5a35438756737e
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-08-25 21:58:18 +0530

    Intersection arraylist

---
+++ src/test/java/ArrayListPractice/ArrayListIntersection.java
@@ -0,0 +1,34 @@
+package ArrayListPractice;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ArrayListIntersection {
+	
+	public static void main(String[] args) {
+		ArrayList<Integer> data1 = new ArrayList<>();
+		data1.add(2);
+		data1.add(3);
+		data1.add(10);
+		ArrayList<Integer> data2 = new ArrayList<>();
+		data2.add(2);
+		data2.add(10);
+		data2.add(3);
+		data2.add(9);
+		
+		Set<Integer> s = new HashSet<>();
+		for(int i=0;i<data2.size();i++) {
+			for(int j=0;j<data1.size();j++) {
+				if(data2.get(i)==data1.get(j)) {
+					s.add(data2.get(i));
+				}
+			}
+		}
+		System.out.println(s);
+		
+		
+		
+	}
+
+}
