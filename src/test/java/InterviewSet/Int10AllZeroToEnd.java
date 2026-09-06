fa244f952eb8dc2e9b45bc50ecdfb01c7a1a8f7c
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-09-06 09:55:24 +0530

    Int 10 Move all zero to end

---
+++ src/test/java/InterviewSet/Int10AllZeroToEnd.java
@@ -0,0 +1,26 @@
+package InterviewSet;
+
+import java.util.Arrays;
+
+public class Int10AllZeroToEnd {
+
+	public static void main(String[] args) {
+		int[] data = {0,1,2,3,0,4,2,0,1,0};
+		
+		System.out.println(Arrays.toString(data));
+		
+		for(int i=0;i<data.length-1;i++) {
+			if(data[i]==0) {
+			for(int j=i+1;j<data.length-1;j++) {
+				if(data[j]!=0) {
+					int temp = data[i];
+					data[i]=data[j];
+					data[j]=temp;
+					
+				}
+			}
+		}
+			}
+		System.out.println(Arrays.toString(data));
+	}
+}
