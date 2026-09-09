96cbf21dd8b4f89271c4f036755856032f3ddae0
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-09-09 18:53:31 +0530

    Int interview Num Palindrom

--- src/test/java/InterviewSet/Int8Palindrom.java
+++ src/test/java/InterviewSet/Int8Palindrom.java
@@ -1,23 +1,50 @@
 package InterviewSet;
 
 public class Int8Palindrom {
+	
+	public static boolean checkPalindrom(int num) {
+		int rev= 0;
+		int temp=num;
+		
+		while(num!=0) {
+			rev = rev*10+num%10;
+			num= num/10;
+		}
+		if(temp==rev) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
 
 	public static void main(String[] args) {
-		int a = 121;
-		int rev = 0;
-		int temp;
-
-		temp = a;
-		while (a != 0) {
-			rev = rev * 10 + a % 10;
-			a = a / 10;
-
+		int data =121;
+		boolean result =checkPalindrom(data);
+		if(result==true) {
+			System.out.println("The Given Num is Palindrom");
+		}else {
+			System.out.println("Given num is not palindrom");
 		}
-		if (temp == rev) {
-			System.out.println("Palindrome");
-		} else {
-			System.out.println("Not a Palindrome");
-		}
+		
+		
+		
+		
+//		int a = 121;
+//		int rev = 0;
+//		int temp;
+//
+//		temp = a;
+//		while (a != 0) {
+//			rev = rev * 10 + a % 10;
+//			a = a / 10;
+//
+//		}
+//		if (temp == rev) {
+//			System.out.println("Palindrome");
+//		} else {
+//			System.out.println("Not a Palindrome");
+//		}
 
 	}
 
