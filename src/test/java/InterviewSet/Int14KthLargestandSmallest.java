20ec3cc7565c59dc0aa616b3e4bef29290b8835b
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-09-10 20:08:59 +0530

    Int 14 kth lar and small

---
+++ src/test/java/InterviewSet/Int14KthLargestandSmallest.java
@@ -0,0 +1,26 @@
+package InterviewSet;
+
+public class Int14KthLargestandSmallest {
+	
+	
+	public static void main(String[] args) {
+		int[] data = {12,34,5,6,1,8,3,4,5,6,7};
+		for(int i=0;i<data.length;i++) {
+			for(int j=i+1;j<data.length;j++) {
+				if(data[i]>=data[j]) {
+					int temp = data[i];
+					data[i]= data[j];
+					data[j]=temp;
+				}
+			}
+		}
+		System.out.println("Kth Largest is: " +data[data.length-3]);
+		System.out.println("Kth Smallest is: " +data[3]);
+		
+		
+		
+		
+		
+	}
+
+}
