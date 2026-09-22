b1b84f3b7de6dfd72536d30d71ddd94f7332098b
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2026-09-22 21:03:47 +0530

    Int 26 Pair multiple equal to given num

---
+++ src/test/java/InterviewSet/Int26MultipleofElementPairlToGiveNum.java
@@ -0,0 +1,28 @@
+package InterviewSet;
+
+public class Int26MultipleofElementPairlToGiveNum {
+	
+	public static void main(String[] args) {
+		int[] data = {2,2,50,48,50,50,100};
+		int target=100;
+		int sum =0;
+		boolean flag=false;
+		
+		for(int i=0;i<data.length;i++) {
+			for(int j=i+1;j<data.length;j++) {
+				sum = data[i]*data[j];
+				if(sum==target) {
+					flag = true;
+					System.out.println("The pair is " +data[i] +" and " +data[j]);
+					break;
+				}
+			}
+			if(flag==true) {
+				break;
+			}
+		}
+	}
+
+
+
+}
